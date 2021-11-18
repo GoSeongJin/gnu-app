bacdc55c945bbf8df17ce23fc41620e06d3261a2
GoSeongJin <91369694+GoSeongJin@users.noreply.github.com>
2021-11-18 21:18:04 +0900

    탭네비

---
+++ src/navigations/Tab.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Home } from '../screens/TabScreens_Home';
+import { Settings } from '../screens/TabScreens_Setting';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const TabIcon = ({ name, size, color }) => {
+  return <MaterialCommunityIcons name={name} size={size} color={color} />;
+};
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => {
+  return (
+    <Tab.Navigator>
+     <Tab.Screen
+        name="공지사항 목록"
+        component={Home}
+        options={{
+          tabBarLabel: '공지사항 목록',
+          tabBarIcon: props =>
+            TabIcon({
+              ...props,
+              name: props.focused ? 'text-box-search' : 'text-box-search-outline',
+            }),
+        }}
+      />
+      <Tab.Screen
+        name="푸시알림 수신"
+        component={Settings}
+        options={{
+          tabBarLabel: '알림 수신 설정',
+          tabBarIcon: props =>
+            TabIcon({
+              ...props,
+              name: props.focused ? 'account-cog' : 'account-cog-outline',
+            }),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigation;
