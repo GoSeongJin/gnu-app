452bbd677bdc27acfd3ccc94eed816750c6f44c8
GoSeongJin <91369694+GoSeongJin@users.noreply.github.com>
2021-11-19 17:33:44 +0900

    DB_UPDATE 클래스화

--- Server_gnu(서버에서 돌릴파일)_Python/DBupdate.py
+++ Server_gnu(서버에서 돌릴파일)_Python/DBupdate.py
@@ -17,89 +17,43 @@ driver = webdriver.Chrome("C:\GO\selenium\chromedriver")
 department_url = "https://newgh.gnu.ac.kr/anse/na/ntt/selectNttList.do?mi=3041&bbsId=1383" #학과공지사항 주소
 Dormitory_url = "https://newgh.gnu.ac.kr/dorm/na/ntt/selectNttList.do?mi=7285&bbsId=2489" #기숙사공지사항 주소
 
-# 웹크롤링시작(학과 공지사항_수강신청)
-driver.get(department_url) #학과공지사항 접속
-select = Select(driver.find_element_by_name("searchType")) # 검색 목록 : 제목
-select.select_by_visible_text('제목')
-elem = driver.find_element_by_name("searchValue") # 검색어 : 수강신청
-elem.send_keys("수강신청")
-elem.send_keys(Keys.RETURN) # 엔터
+class Crawler :
 
-time.sleep(2)
-number = driver.find_elements_by_css_selector("td.BD_tm_none")[0].text #게시글 번호
-Title = driver.find_elements_by_css_selector(".nttInfoBtn")[0].text #게시글 제목
-driver.find_elements_by_css_selector(".nttInfoBtn")[0].click() #접속
-time.sleep(2)
-url_S = driver.current_url #현재 게시글 주소
+  def __init__(self):
+    self.Posting_Address = 0
+    
+  def crawler(self, Homepage_name, Homepage_url ,searchValue):
+    driver.get(Homepage_url) #학과공지사항 또는 기숙사 공지사항 접속
+    select = Select(driver.find_element_by_name("searchType")) # 검색 목록 : 제목
+    select.select_by_visible_text('제목')
+    elem = driver.find_element_by_name("searchValue") # 검색어 : searchValue
+    elem.send_keys(searchValue)
+    elem.send_keys(Keys.RETURN) # 엔터
+    time.sleep(2)
+    number = driver.find_elements_by_css_selector("td.BD_tm_none")[0].text #게시글 번호
+    Title = driver.find_elements_by_css_selector(".nttInfoBtn")[0].text #게시글 제목
+    driver.find_elements_by_css_selector(".nttInfoBtn")[0].click() #접속
+    time.sleep(2)
+    self.Posting_Address = driver.current_url #현재 게시글 주소
+    db = firebase.database()
+    DB_lastest = db.child( Homepage_name + "/" + searchValue ).order_by_key().limit_to_last(1).get()
 
-db = firebase.database()
-DB_lastest = db.child("학과공지사항/수강신청").order_by_key().limit_to_last(1).get()
-for data in DB_lastest.each():
-    DB_lastest_key = data.key()
-    DB_lastest_url = data.val()['주소'] #DB 게시글 주소
+    for data in DB_lastest.each():
+        DB_lastest_url = data.val()['주소'] #DB 게시글 주소
 
-if(url_S != DB_lastest_url) :
-  input_data = {"제목": Title , "주소" : url_S}
-  db.child("학과공지사항/수강신청").child(number).set(input_data)
-  driver.back() #뒤로가기
-  time.sleep(2)
-  print("업데이트가 되었습니다")
+    if(self.Posting_Address != DB_lastest_url) :
+      input_data = {"제목": Title , "주소" : self.Posting_Address}
+      db.child( Homepage_name + "/" + searchValue).child(number).set(input_data)
+      driver.back() #뒤로가기
+      time.sleep(2)
+      print("업데이트가 되었습니다")
+    return self.Posting_Address  
 
-# 웹크롤링시작(학과 공지사항_장학금)
-driver.get(department_url) #학과공지사항 접속
-select = Select(driver.find_element_by_name("searchType")) # 검색 목록 : 제목
-select.select_by_visible_text('제목')
-elem = driver.find_element_by_name("searchValue") # 검색어 : 장학금
-elem.send_keys("장학금")
-elem.send_keys(Keys.RETURN) # 엔터
+cal1 = Crawler()
+cal1.crawler("학과공지사항", department_url, "수강신청")
+cal1.crawler("학과공지사항", department_url, "장학금")
+cal1.crawler("기숙사공지사항", Dormitory_url, "입실")
 
-time.sleep(2)
-number = driver.find_elements_by_css_selector("td.BD_tm_none")[0].text #게시글 번호
-Title = driver.find_elements_by_css_selector(".nttInfoBtn")[0].text #게시글 제목
-driver.find_elements_by_css_selector(".nttInfoBtn")[0].click() #접속
-time.sleep(2)
-url_S = driver.current_url #현재 게시글 주소
-
-db = firebase.database()
-DB_lastest = db.child("학과공지사항/장학금").order_by_key().limit_to_last(1).get()
-for data in DB_lastest.each():
-    DB_lastest_key = data.key()
-    DB_lastest_url = data.val()['주소'] #DB 게시글 주소
-
-if(url_S != DB_lastest_url) :
-  input_data = {"제목": Title , "주소" : url_S}
-  db.child("학과공지사항/장학금").child(number).set(input_data)
-  driver.back() #뒤로가기
-  time.sleep(2)
-  print("업데이트가 되었습니다")
-
-# 웹크롤링시작(기숙사 공지사항_입실)
-driver.get(Dormitory_url) #기숙사공지사항 접속
-select = Select(driver.find_element_by_name("searchType")) # 검색 목록 : 제목
-select.select_by_visible_text('제목')
-elem = driver.find_element_by_name("searchValue") # 검색어 : 입실
-elem.send_keys("입실")
-elem.send_keys(Keys.RETURN) # 엔터
-
-time.sleep(2)
-number = driver.find_elements_by_css_selector("td.BD_tm_none")[0].text #게시글 번호
-Title = driver.find_elements_by_css_selector(".nttInfoBtn")[0].text #게시글 제목
-driver.find_elements_by_css_selector(".nttInfoBtn")[0].click() #접속
-time.sleep(2)
-url_S = driver.current_url #현재 게시글 주소
-
-db = firebase.database()
-DB_lastest = db.child("기숙사공지사항/입실").order_by_key().limit_to_last(1).get()
-for data in DB_lastest.each():
-    DB_lastest_key = data.key()
-    DB_lastest_url = data.val()['주소'] #DB 게시글 주소
-
-if(url_S != DB_lastest_url) :
-  input_data = {"제목": Title , "주소" : url_S}
-  db.child("기숙사공지사항/입실").child(number).set(input_data)
-  driver.back() #뒤로가기
-  time.sleep(2)
-  print("업데이트가 되었습니다")
 driver.close()
 
 
