e8559dc7d291d3d94f70611b49e34fe049fa5894
seongjin17 <62363515+seongjin17@users.noreply.github.com>
2021-11-19 22:08:10 +0900

    Add files via upload

--- Server_gnu(서버에서 돌릴파일)_Python/DBupdate.py
+++ Server_gnu(서버에서 돌릴파일)_Python/DBupdate.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from onesignal_sdk.client import Client
 import time
 import pyrebase
 
@@ -11,7 +12,14 @@ config = {
   "storageBucket": "test-f972d.appspot.com"
 }
 
+notification_body = {
+    'contents': {'tr': '알림을 눌러 접속하세요','en': Title}, # 메세지 내용 설정
+    'included_segments': [searchValue],
+    'app_url' : url_S
+}
+
 firebase = pyrebase.initialize_app(config)
+client = Client(app_id = "11d78e2a-f43d-49f4-ae46-c11fbac37252", rest_api_key = "MTVlZGZmYzAtNGIyMC00YjQwLTkxYzktYzk3Y2I4MDMyYmRl", user_auth_key = "Y2IwYTU3NWMtOWZiMy00YWQxLWJlMzEtNmIwMjI2N2MzNzRm")
 
 driver = webdriver.Chrome("C:\GO\selenium\chromedriver")
 department_url = "https://newgh.gnu.ac.kr/anse/na/ntt/selectNttList.do?mi=3041&bbsId=1383" #학과공지사항 주소
@@ -38,6 +46,12 @@ class Crawler :
     db = firebase.database()
     DB_lastest = db.child( Homepage_name + "/" + searchValue ).order_by_key().limit_to_last(1).get()
 
+    notification_body = {
+    'contents': {'tr': '알림을 눌러 접속하세요','en': Title}, # 메세지 내용 설정
+    'included_segments': [searchValue],
+    'app_url' : self.Posting_Address
+}
+
     for data in DB_lastest.each():
         DB_lastest_url = data.val()['주소'] #DB 게시글 주소
 
@@ -46,7 +60,7 @@ class Crawler :
       db.child( Homepage_name + "/" + searchValue).child(number).set(input_data)
       driver.back() #뒤로가기
       time.sleep(2)
-      print("업데이트가 되었습니다")
+      client.send_notification(notification_body)
     return self.Posting_Address 
 cal1 = Crawler()
 cal1.crawler("학과공지사항", department_url, "수강신청")
