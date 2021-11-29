5954b4fd4edef1aeaf9de4d83dcb529f97179e83
GoSeongJin <91369694+GoSeongJin@users.noreply.github.com>
2021-11-29 09:30:36 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,23 @@ import React,{useEffect}  from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import OneSignal from 'react-native-onesignal';
 import { initializeApp } from "firebase/app";
-// import TabNavigation from './navigations/Tab';
 import StackNavigation from './navigations/Stack';
 
  const App = () => {
    useEffect(()=>{
      OneSignal.setLogLevel(6, 0);
-     OneSignal.setAppId("a65f8dfe-7203-465d-9ac5-8361b0c35b90");
+     OneSignal.setAppId("11d78e2a-f43d-49f4-ae46-c11fbac37252");
      OneSignal.setNotificationOpenedHandler(notification => {
        console.log("OneSignal: notification opened:", notification);
      });
      const firebaseConfig = {
-      apiKey: "AIzaSyB0FQtNGUseSlHoFX5PZ4a91tRSXTyZp7w",
-      authDomain: "test-f972d.firebaseapp.com",
-      databaseURL: "https://test-f972d-default-rtdb.firebaseio.com",
-      projectId: "test-f972d",
-      storageBucket: "test-f972d.appspot.com",
-      messagingSenderId: "478142633493",
-      appId: "1:478142633493:web:3e3fcb9e7ca703b0ade01c"
+      apiKey: "AIzaSyAHYW60cI1kChuFx3Z__DeLvKBGyrGLkZg",
+      authDomain: "gnu6-d9c5b.firebaseapp.com",
+      databaseURL: "https://gnu6-d9c5b-default-rtdb.firebaseio.com",
+      projectId: "gnu6-d9c5b",
+      storageBucket: "gnu6-d9c5b.appspot.com",
+      messagingSenderId: "869224764382",
+      appId: "1:869224764382:web:7fbf1ea1dcf195b921034f"
     };
     const app = initializeApp(firebaseConfig);
    },[])
